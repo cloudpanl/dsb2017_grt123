a0738a8c3da1b113c593e7e41287c9578cef81d6
X250 <dsfsd@163.com>
2018-04-23 15:14:25 +0800

    changed config_training

--- training/config_training.py
+++ training/config_training.py
@@ -2,11 +2,11 @@ config = {'stage1_data_path':'/data/lungCT/dsb2017/sample_images',
           'luna_raw':'/data/lungCT/luna/raw',
           'luna_segment':'/data/lungCT/luna/seg-lungs-LUNA16',
           
-          'luna_data':'/data/lungCT/luna/allset',
-          'preprocess_result_path':'/data/lungCT/dsb2017/generation_data/',       
+          'luna_data':'/data/lungCT/luna/temp/rename_data',
+          'preprocess_result_path':'/data/lungCT/luna/temp/luna_npy',       
           
           'luna_abbr':'./detector/labels/shorter.csv',
-          'luna_label':'./detector/labels/lunaqualified.csv',
+          'luna_label':'/data/lungCT/luna/annotations.csv',
           'stage1_annos_path':['./detector/labels/label_job5.csv',
                 './detector/labels/label_job4_2.csv',
                 './detector/labels/label_job4_1.csv',
@@ -15,3 +15,6 @@ config = {'stage1_data_path':'/data/lungCT/dsb2017/sample_images',
           'bbox_path':'../detector/results/res18/bbox/',
           'preprocessing_backend':'python'
          }
+
+
+#'luna_label':'./detector/labels/lunaqualified.csv',
